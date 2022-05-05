3a6153413b94dc7a2a345dfc9740e2509c83caea
mletizia <marco.letizia@edu.unige.it>
2022-05-05 17:20:36 +0200

    Add files via upload

---
+++ MMD.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from sklearn.gaussian_process.kernels import RBF
+
+from scipy.spatial.distance import pdist
+
+
+def md_pairwise(data):
+    '''
+    to estimate the bandwidth
+    '''
+
+    return np.median(pdist(data))
+
+
+
+def MMD2(x,y,sigma=1):
+    '''
+    from https://www.jmlr.org/papers/volume13/gretton12a/gretton12a.pdf  (Eq.3)
+
+    '''
+    k =  RBF(length_scale=sigma)
+
+    lx = len(x)
+    ly = len(y)
+
+    MMD2 = 1/(lx * (lx-1)) * (k(x,x).sum()-k(x,x).trace()) + 1/(ly * (ly-1)) * (k(y,y).sum()-k(y,y).trace()) - 2/(lx * ly) * k(x,y).sum()
+
+    return MMD2
