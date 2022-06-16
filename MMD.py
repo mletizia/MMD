449bd14f9bcee4baf66b50d214c6af454ae83fd5
mletizia <marco.letizia@edu.unige.it>
2022-06-16 18:50:58 +0200

    added biased empirical estimate MMDb of MMD

--- MMD.py
+++ MMD.py
@@ -27,3 +27,18 @@ def MMD2(x,y,sigma=1):
     MMD2 = 1/(lx * (lx-1)) * (k(x,x).sum()-k(x,x).trace()) + 1/(ly * (ly-1)) * (k(y,y).sum()-k(y,y).trace()) - 2/(lx * ly) * k(x,y).sum()
 
     return MMD2
+
+
+def MMDb(x,y,sigma=1):
+    '''
+    from https://www.jmlr.org/papers/volume13/gretton12a/gretton12a.pdf  (Eq.5)
+
+    '''
+    k =  RBF(length_scale=sigma)
+
+    lx = len(x)
+    ly = len(y)
+
+    MMDb = np.sqrt((1/lx^2) * k(x,x).sum() + (1/ly^2) * k(y,y).sum() - 2/(lx * ly) * k(x,y).sum())
+
+    return MMDb
